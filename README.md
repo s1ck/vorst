e56372e9b899799791397299531dd3979e4ebef3
Martin Junghanns <martin.junghanns@neo4j.com>
2021-01-27 22:22:04 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+## vorst
+
+vorst is a visualization of sorting algorithms for the console.
+
+It is inspired by [Crust of Rust: Sorting Algorithms](https://www.youtube.com/watch?v=h4RkCyJyXmM) by [Jon Gjengset](https://github.com/jonhoo).
+
+License: MIT OR Apache-2.0
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//! # vorst
+//!
+//! vorst is a visualization of sorting algorithms for the console.
+//!
+//! It is inspired by [Crust of Rust: Sorting Algorithms](https://www.youtube.com/watch?v=h4RkCyJyXmM) by [Jon Gjengset](https://github.com/jonhoo).
 use itertools::{EitherOrBoth, Itertools};
 use std::error::Error;
 use std::io::{stdout, Stdout};
